0e31cb1e909c9e13866b2d0f315d0a58b2269b03
Luckbox <lucas.muoz@uc.cl>
2022-07-26 16:13:18 -0400

    refact: deleted public API Key

--- heroApi.py
+++ heroApi.py
@@ -1,9 +1,10 @@
 import requests
+from decouple import config
 from hero import Hero
 
 class HeroAPI:
     def __init__(self):
-        self.url = 'https://www.superheroapi.com/api.php/5281794541934977/'
+        self.url = 'https://www.superheroapi.com/api.php/'+ config("HERO_API_KEY") + '/'
 
     def getHero(self, id):
         response = requests.get(self.url + str(id))
